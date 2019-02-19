e4c85ae2aa784918f5c5134d1ef924c2c9b3e200
David Collignon <david.s.collignon@gmail.com>
2019-02-19 17:33:05 +0100

    odifié la route, pour l'ajouter dans la syntaxe resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get    "events/:id",      to: "events#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :events, only: [:new, :create, :destroy]
+  resources :events, only: [:show, :new, :create, :destroy]
 end
