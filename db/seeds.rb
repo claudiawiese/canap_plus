d03133159d9124e4523043429f31f711fa0fac33
David Collignon <david.s.collignon@gmail.com>
2019-02-19 15:46:35 +0100

    commit test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do
+user = User.new(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  email: Faker::Internet.email,
+  nickname: Faker::JapaneseMedia::DragonBall.character,
+  phone: Faker::PhoneNumber.phone_number,
+  team: Faker::Football.team,
+  bio: Faker::GreekPhilosophers.quote,
+  birthday: Faker::Date.between(80.years.ago, 16.years.ago)
+  )
+end
+
+
