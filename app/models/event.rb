f4df64851af3cd4c6afa37bda1c270f2ee32a25e
Claudia Wiese <claudia.wiese@sciencespo.fr>
2019-02-22 15:19:45 +0100

    event form validations fixed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,13 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
+  validates :game, presence: true
+  validates :description, presence: true
+  validates :date, presence: true
   validates :capacity, presence: true
+  validates :address, presence: true
+   validates :fee, presence: true
+  validates :photo, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   mount_uploader :photo, PhotoUploader
